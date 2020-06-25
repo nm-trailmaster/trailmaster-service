9d727d1c32d8c15b12422d6ea6eb6f9a1312d4e5
Austin Preston <austinpreston314@gmail.com>
2020-06-25 14:31:05 -0600

    Added Repository classes.

--- src/main/java/edu/cnm/deepdive/trailmasterservice/service/PhotoRepository.java
+++ src/main/java/edu/cnm/deepdive/trailmasterservice/service/PhotoRepository.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.trailmasterservice.service;
 import edu.cnm.deepdive.trailmasterservice.model.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Unsure how the upload process works and what it is needed
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
   Iterable<Photo> getAllByOrderByTextAsc();
