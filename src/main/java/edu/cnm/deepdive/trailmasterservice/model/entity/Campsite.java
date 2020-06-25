2cdb2a3680312466a1ee5d14150fca00d97add9b
Austin Preston <austinpreston314@gmail.com>
2020-06-25 11:06:12 -0600

    Added Entity classes.

--- src/main/java/edu/cnm/deepdive/trailmasterservice/model/entity/Campsite.java
+++ src/main/java/edu/cnm/deepdive/trailmasterservice/model/entity/Campsite.java
@@ -1,5 +1,87 @@
 package edu.cnm.deepdive.trailmasterservice.model.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.NonNull;
+
+@SuppressWarnings("JpaDataSourceORMInspection")
+@Entity
 public class Campsite {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "campsite_id", nullable = false, updatable = false)
+  private Long id;
+
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  private Date created;
+
+  @UpdateTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date updated;
+
+  @Column(nullable = false)
+  private double latitude;
+
+  @Column(nullable = false)
+  private double longitude;
+
+  @Column(nullable = false)
+  private int rating;
+
+  @NonNull
+  @Column(length =5_000, nullable = false)
+  private String comment;
+
+  public Long getId() {
+    return id;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  public Date getUpdated() {
+    return updated;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(double latitude) {
+    this.latitude = latitude;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(double longitude) {
+    this.longitude = longitude;
+  }
+
+  @NonNull
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(@NonNull String comment) {
+    this.comment = comment;
+  }
 }
--- src/main/java/edu/cnm/deepdive/trailmasterservice/model/entity/Photo.java
+++ src/main/java/edu/cnm/deepdive/trailmasterservice/model/entity/Photo.java
@@ -1,5 +1,91 @@
 package edu.cnm.deepdive.trailmasterservice.model.entity;
 
+import java.io.File;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.NonNull;
+
+@SuppressWarnings("JpaDataSourceORMInspection")
+@Entity
 public class Photo {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "photo_id", nullable = false, updatable = false)
+  private Long id;
+
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date created;
+
+  @UpdateTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date updated;
+
+  @NonNull // Subject to change along with respective getter and setter.
+  @Column(nullable = false)
+  private File filepath;
+
+  @ManyToOne(fetch = FetchType.EAGER,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinTable(name = "campsite_id")
+  private Campsite campsite;
+
+  @ManyToOne(fetch = FetchType.EAGER,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinTable(name = "trail_id")
+  private Trail trail;
+
+  public Long getId() {
+    return id;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  public Date getUpdated() {
+    return updated;
+  }
+
+  @NonNull
+  public File getFilepath() {
+    return filepath;
+  }
+
+  public void setFilepath(@NonNull File filepath) {
+    this.filepath = filepath;
+  }
+
+  public Campsite getCampsite() {
+    return campsite;
+  }
+
+  public void setCampsite(Campsite campsite) {
+    this.campsite = campsite;
+  }
+
+  public Trail getTrail() {
+    return trail;
+  }
+
+  public void setTrail(Trail trail) {
+    this.trail = trail;
+  }
 }
