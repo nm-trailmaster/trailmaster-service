6abad7f6fa4d2edf2bccc5f2b95e362977aeb53f
Austin Preston <austinpreston314@gmail.com>
2020-07-08 11:30:49 -0600

    Made project runnable

--- src/main/java/edu/cnm/deepdive/trailmasterservice/service/PhotoRepository.java
+++ src/main/java/edu/cnm/deepdive/trailmasterservice/service/PhotoRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //Unsure how the upload process works and what it is needed
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
-  Iterable<Photo> getAllByOrderByTextAsc();
-
   Iterable<Photo> getAllByCampsiteOrderByCreatedAsc(Campsite campsite);
 
   Iterable<Photo> getAllByTrailOrderByCreatedAsc(Trail trail);
